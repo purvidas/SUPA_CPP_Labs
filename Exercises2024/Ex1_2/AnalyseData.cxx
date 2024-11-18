1e5d596c5575773330b53f954a0fab544ad09834
Purvi Das <152137770+purvidas@users.noreply.github.com>
2024-11-18 16:16:45 +0000

    Updating AnalyseData.cxx with Instruction 2

--- Exercises2024/Ex1_2/AnalyseData.cxx
+++ Exercises2024/Ex1_2/AnalyseData.cxx
@@ -4,16 +4,31 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <vector> 
+#include <utility>
 
 using namespace std; // instead of std:: 
 
 int main(){
+    // ifstream file("input2D_float.txt"); 
+
+
     ifstream file("input2D_float.txt"); 
+    vector<pair<double, double>> dataset;
 
     string line;
     while(getline(file, line)){
-        cout << '(' << line << ')' << '\n';
-    }
+        stringstream s(line);
+        double x, y;
+        char c;
+        while(s >> x >> c >> y){
+            dataset.emplace_back(x, y);
+            }
+        }
     file.close();
+
+    for (const auto& [x, y]: dataset){
+        cout << '(' << x << ',' << y << ')' << '\n';
+    }
     return 0;
 }
