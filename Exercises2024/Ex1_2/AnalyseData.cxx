a2c16e06baee42cb29f74505058c24ab7d10c909
Purvi Das <152137770+purvidas@users.noreply.github.com>
2024-11-19 16:48:33 +0000

    Updating AnalyseData.cxx with Instruction 6

--- Exercises2024/Ex1_2/AnalyseData.cxx
+++ Exercises2024/Ex1_2/AnalyseData.cxx
@@ -1,61 +1,23 @@
 // Purvi Das, Nov 13, 2024
 
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <vector> 
-#include <utility>
-#include <cmath>
-
-using namespace std; // instead of std:: 
-
-vector<pair<double, double>> fileread(const string& input_file){ // string input 
-    ifstream file(input_file); 
-    vector<pair<double, double>> dataset;
-    string line;
-    while(getline(file, line)){
-        stringstream s(line);
-        double x, y;
-        char c;
-        while(s >> x >> c >> y){ // because of ,
-            dataset.emplace_back(x, y); // add directly to the vector
-            }
-        }
-    return dataset;
-}
-
-vector<double> magnitude(const vector<pair<double, double>>& dataset){
-    vector<double> m; 
-    for(const auto& [x, y] : dataset){
-        m.push_back(sqrt(pow(x, 2) + pow(y, 2)));
-    }
-    return m; 
-}
-
-void print(const vector<pair<double, double>>& dataset, vector<double> magnitude, int n){
-    if (n < dataset.size()){
-        for(int i = 0; i < n && i < dataset.size(); i++){
-            const auto& [x, y] = dataset[i];
-            // const ensures the elements are not modified 
-            // auto to know the type automatically 
-            cout << '(' << x << ',' << y << ')' << " = " << magnitude[i] << '\n';
-        }
-    } else {
-        cout << "Warning! " << '\n';
-        for(int i = 0; i < 5 ; i++){
-            const auto& [x, y] = dataset[i];
-            cout << '(' << x << ',' << y << ')' << " = " << magnitude[i] << '\n';
-        }
-    }   
-}
+#include "CustomFunctions.h"
 
 int main(){
     vector<pair<double, double>> dataset = fileread("input2D_float.txt");
-    int n;
+    vector<double> magnitude_output = magnitude(dataset);
+    int n, output; 
+
+    cout << "1. Dataset \n2. Dataset with Magnitude \nWhat do you want?: ";
+    cin >> output; 
     cout << "Provide the number of lines you want to print: ";
     cin >> n;
-    vector<double> magnitude_data = magnitude(dataset);
-    print(dataset, magnitude_data, n);
+
+    if(output == 1){
+        print(dataset, magnitude_output, n, false);
+    }
+    else{
+        print(dataset, magnitude_output, n, true);               
+    }
     return 0;
 }
